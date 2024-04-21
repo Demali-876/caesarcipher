ecb4c148fae3f78726da20bd1a004a52781f0211
Demali-876 <90882773+Demali-876@users.noreply.github.com>
2024-04-20 21:48:01 -0500

    Final integrations

--- src/caesarcipher_frontend/src/routes/+page.svelte
+++ src/caesarcipher_frontend/src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script>
   import "../index.css";
   import { backend } from '$lib/canisters';
+  import { onMount } from 'svelte';
+
 
   let message = '';
   let shift = 0;
   let foreignChar = { Exclude: null };
   let sensitivity = { Ignore: null };
   let output = '';
+  let outputLetter = 'a';
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let activeAction = 'encode';
+  $: encodedChars = message.length;
 
   function updateVariantSelection(event, variantType) {
     if (variantType === 'foreignChar') {
@@ -15,8 +21,29 @@
       sensitivity = { [event.target.value]: null };
     }
   }
+  function updateOutputLetter() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const index = (shift % 26 + 26) % 26;
+    outputLetter = alphabet[index];
+  }
+  $: updateOutputLetter(), shift;
+  function stepUp() {
+    shift += 1;
+  }
 
+  function stepDown() {
+    shift -= 1;
+  }
+  function clearMessage() {
+    message = '';
+  }
+  function resetControl(){
+    shift = 0;
+    foreignChar = { Include: null };
+    sensitivity = { MaintainCase: null };
+  }
   async function encodeMessage() {
+    activeAction = 'encode';
     try {
       const result = await backend.encode({
         message,
@@ -24,7 +51,7 @@
         foreignChar,
         sensitivity
       });
-      output = 'Encoded message :' + result;
+      output = result;
     } catch (error) {
       console.error('Error encoding message:', error);
       output = 'Failed to encode message.';
@@ -32,6 +59,7 @@
   }
 
   async function decodeMessage() {
+    activeAction = 'decode';
     try {
       const result = await backend.decode({
         message,
@@ -39,12 +67,15 @@
         foreignChar,
         sensitivity
       });
-      output = 'Decoded message: ' + result;
+      output = result;
     } catch (error) {
       console.error('Error decoding message:', error);
       output = 'Failed to decode message.';
     }
   }
+  onMount(() => {
+    updateOutputLetter();
+  });
 </script>
 <div class="app">
   <header class="app__header header">
@@ -52,46 +83,15 @@
       <div class="header__item">
         <a class="header__brand" href="/">
           <!-- svelte-ignore a11y-missing-attribute -->
-          <img width="125" height="200"  aria-labelledby="app_logo" src="logo.svg">
+          <img class=" small-on-mobile"width="125" height="150"  aria-labelledby="app_logo" src="logo.svg">
         </a>
       </div>
-      
-
     </div>
   </header>
-
   <article class="app__pipe pipe">
-    <header class="pipe__header">
-      <div class="container">
-        <h1 class="pipe__title">Caesar cipher: Encode and decode online</h1>
-        <p class="pipe__description">Method in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. The method is named after Julius Caesar, who used it in his private correspondence.</p>
-        <div class="badge">
-            <a class="badge__link" href="https://ciphereditor.com/explore/caesar-cipher?utm_campaign=badge_open_in_ciphereditor&amp;utm_source=cryptii&amp;utm_medium=referral" target="_blank" rel="noopener">
-              <img class="badge__image" alt="internet computer" width="196" height="56" src="logo2.svg" loading="lazy">
-            </a>
-          </div>
-        <ul class="pipe__link-list link-list">
-          <li class="link-list__item">
-              <a class="link-list__link" href="/pipes/decimal-text">
-                Decimal to text
-              </a>
-            </li><li class="link-list__item">
-              <a class="link-list__link" href="/pipes/urlencode">
-                URL encode
-              </a>
-            </li><li class="link-list__item">
-              <a class="link-list__link" href="/pipes/enigma-decoder">
-                Enigma decoder
-              </a>
-        </ul>
-      </div>
-    </header>
-    
   <div class="pipe__scrollable">
     <div class="pipe__content">
       <button class="pipe__part-pipe">
-        <div class="pipe__btn-add">Add encoder or viewer</div>
-        <div class="pipe__drop-handle"></div>
       </button>
       <div class="pipe__part-brick pipe__part-brick--viewer" draggable="true">
         <div class="brick viewer viewer-text" role="region" aria-label="Text viewer">
@@ -107,13 +107,14 @@
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3 6l5 5 5-5z"></path></svg>
               </div>
             </button>
-            <button class="brick__btn-menu">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><circle cx="8" cy="2" r="1.5"></circle><circle cx="8" cy="8" r="1.5"></circle><circle cx="8" cy="14" r="1.5"></circle></svg></button>
+            <button class="brick__btn-menu brick__action" on:click={clearMessage}>
+            Clear
+            </button>
             </header>
             <div class="brick__settings">
               <div class="form"></div>
             </div><div class="brick__content">
-              <textarea class="viewer-text__textarea" aria-label="Content" spellcheck="false" style="height: 200px;"></textarea>
+              <textarea class="viewer-text__textarea" aria-label="Content" spellcheck="false" style="height: 200px;"bind:value={message}></textarea>
             </div>
             <footer class="brick__status brick__status--hidden">
               <div class="brick__status-icon"></div>
@@ -123,68 +124,68 @@
           </div>
         </div>
         <button class="pipe__part-pipe">
-          <div class="pipe__btn-add">Add encoder or viewer</div>
-          <div class="pipe__drop-handle">
-          </div>
         </button>
         <div class="pipe__part-brick pipe__part-brick--encoder" draggable="true">
           <div class="brick encoder" role="region" aria-label="Caesar cipher encoder">
             <header class="brick__header">
-              <!-- svelte-ignore a11y-invalid-attribute -->
               <ul class="brick__actions">
                 <li class="brick__action-item">
-                  <!-- svelte-ignore a11y-invalid-attribute -->
-                  <a class="brick__action brick__action--active" href="#" draggable="false">Encode</a>
+                  <a href="#" class="brick__action" class:brick__action--active={activeAction === 'encode'} draggable="false" on:click={encodeMessage}>
+                    Encode
+                  </a>
                 </li>
-                <li class="brick__action-item"><a class="brick__action" href="#" draggable="false">Decode</a>
+                <li class="brick__action-item">
+                  <a href="#" class="brick__action" class:brick__action--active={activeAction === 'decode'} draggable="false" on:click={decodeMessage}>
+                    Decode
+                  </a>
                 </li>
               </ul>
-              <button class="brick__title"><h3 class="brick__title-inner">Caesar cipher</h3>
+              <button class="brick__title">
+                <h3 class="brick__title-inner">Caesar cipher</h3>
                 <div class="brick__title-caret">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3 6l5 5 5-5z"></path></svg>
                 </div>
               </button>
-              <button class="brick__btn-menu"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><circle cx="8" cy="2" r="1.5"></circle><circle cx="8" cy="8" r="1.5"></circle><circle cx="8" cy="14" r="1.5"></circle></svg>
-              </button>
+              <button class="brick__btn-menu brick__action " on:click={resetControl}>Reset</button>
             </header>
             <div class="brick__settings">
               <div class="form">
                 <div class="field field-number field--first" data-width="12">
                   <label class="field__label" for="u1">Shift</label>
                   <div class="field-number__field">
-                    <button class="field-number__btn-step-down">Step Down</button>
+                    <button class="field-number__btn-step-down" on:click={stepDown}>Step Down</button>
                     <div class="field-number__value">
-                      <input class="field-number__input" id="u1" type="number">
+                      <input class="field-number__input" id="u1" type="number" bind:value={shift} >
                       <div class="field-number__display field-number__display--enabled">
-                        <span class="field-number__display-value">7</span>
-                        <span class="field-number__display-description">a→h</span>
+                        <span class="field-number__display-value">{shift}</span>
+                        <span class="field-number__display-description">a→{outputLetter}</span>
                       </div>
                     </div>
-                    <button class="field-number__btn-step-up">Step Up</button>
+                    <button class="field-number__btn-step-up" on:click={stepUp}>Step Up</button>
                   </div>
                 </div>
                 <div class="field field-text field--first" data-width="12">
                   <label class="field__label" for="u2">Alphabet</label>
                   <div class="field-text__field">
-                    <input class="field-text__input" id="u2" type="text" spellcheck="false">
+                    <input class="field-text__input" id="u2" type="text" bind:value={alphabet} spellcheck="false" readonly>
                   </div>
                 </div>
                 <div class="field field-enum field-enum--default field--first" data-width="6">
-                  <label class="field__label" for="u3">Case Strategy</label>
+                  <label class="field__label" for="u3">Case Sensitivity</label>
                   <div class="field-enum__field">
-                    <select class="field-enum__select" id="u3">
-                      <option value="0" title="">Maintain case</option>
-                      <option value="1" title="">Ignore case</option>
+                    <select class="field-enum__select" id="u3" on:change={(e) => updateVariantSelection(e, 'sensitivity')}>
+                      <option value="MaintainCase" title="">Maintain case</option>
+                      <option value="Ignore" title="">Ignore case</option>
                     </select>
                   </div>
                 </div>
-                <div class="field field-boolean" data-width="6">
-                  <label class="field__label" for="u4">Foreign Chars</label>
-                  <div class="field__field">
-                    <input class="field-boolean__input" type="checkbox" id="u4">
-                    <label class="field-boolean__toggle" for="u4"><span class="field-boolean__choice">Include</span>
-                      <span class="field-boolean__choice">Ignore</span>
-                    </label>
+                <div class="field field-enum field-enum--default field--firs" data-width="6">
+                  <label class="field__label" for="u3">Foreign Chars</label>
+                  <div class="field-enum__field">
+                    <select class="field-enum__select" id="u3" on:change={(e) => updateVariantSelection(e, 'foreignChar')}>
+                      <option value="Include">Include</option>
+                      <option value="Exclude">Exclude</option>
+                    </select>
                   </div>
                 </div>
               </div>
@@ -193,13 +194,11 @@
               <div class="brick__status-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M16 7H3.83l3.58-3.59L6 2 0 8l6 6 1.41-1.41L3.83 9H16z"></path></svg>
               </div>
-              <div class="brick__status-message">Encoded 163 chars</div>
+              <div class="brick__status-message">Encoded {encodedChars} chars</div>
             </footer>
           </div>
         </div>
         <button class="pipe__part-pipe">
-          <div class="pipe__btn-add">Add encoder or viewer</div>
-          <div class="pipe__drop-handle"></div>
         </button><div class="pipe__part-brick pipe__part-brick--viewer" draggable="true">
           <div class="brick viewer viewer-text" role="region" aria-label="Text viewer">
             <header class="brick__header">
@@ -213,15 +212,14 @@
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M3 6l5 5 5-5z"></path></svg>
                 </div>
               </button>
-              <button class="brick__btn-menu">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><circle cx="8" cy="2" r="1.5"></circle><circle cx="8" cy="8" r="1.5"></circle><circle cx="8" cy="14" r="1.5"></circle></svg>
-              </button>
+              <button class="brick__btn-menu-canister brick__action ">
+                Canister Interface</button>
             </header>
             <div class="brick__settings">
               <div class="form"></div>
             </div>
             <div class="brick__content">
-              <textarea class="viewer-text__textarea" aria-label="Content" spellcheck="false" style="height: 200px;"></textarea>
+              <textarea class="viewer-text__textarea" aria-label="Content" spellcheck="false" style="height: 200px;">{output}</textarea>
             </div>
             <footer class="brick__status brick__status--hidden">
               <div class="brick__status-icon"></div>
@@ -230,12 +228,32 @@
           </div>
         </div>
         <button class="pipe__part-pipe">
-          <div class="pipe__btn-add">Add encoder or viewer</div>
-          <div class="pipe__drop-handle"></div>
         </button>
       </div>
       <div class="pipe__scroll-handle pipe__scroll-handle--left pipe__scroll-handle--disabled"></div>
       <div class="pipe__scroll-handle pipe__scroll-handle--right pipe__scroll-handle--disabled"></div>
     </div>
+    <header class="pipe__header">
+      <div class="container">
+        <h1 class="pipe__title">Caesar cipher: Encode and decode canister service</h1>
+        <p class="pipe__description">Method in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. The method is named after Julius Caesar, who used it in his private correspondence.</p>
+        <div class="badge">
+            <a class="badge__link" href="https://internetcomputer.org/" target="_blank" rel="noopener">
+              <img class="badge__image" alt="internet computer" width="196" height="56" src="logo2.svg" loading="lazy">
+            </a>
+          </div>
+        <ul class="pipe__link-list link-list">
+          <li class="link-list__item">
+              <a class="link-list__link" href="https://oajhk-xaaaa-aaaap-qca7a-cai.icp0.io/">
+                Creator
+              </a>
+            </li><li class="link-list__item">
+              <a class="link-list__link" href="https://twitter.com/demali_icp">
+                Twitter
+              </a>
+            </li>
+        </ul>
+      </div>
+    </header>
   </article>
 </div>
